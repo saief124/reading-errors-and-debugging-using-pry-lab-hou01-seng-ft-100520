55eb5532c46d32c015f6861bb1bdb778d0317a34
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-01 20:33:33 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,24 +1,31 @@
-# don't forget to add: require 'pry'
-
-def get_user_input
+require 'pry'
+def get_user_input # method to read the user input
   gets.chomp
 end
 
-def prompt_user
+def prompt_user # method to ask user for an input
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
 
-def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+def selection(num) # using the variable num as argument for the method
+
+prompt_user
+num=get_user_input
+  binding.pry
+  if num == 1 # checking if user input is 1
+      s = puts "YUM YUM MUNCH MUNCH MUNCH" # passing output to a variable depending on the input 1
+selection(num)==s
+
+  elsif num == 2 # checking if user input is 1
+
+    s = puts "HAM HAM HAM IN MY TUMMY" # passing output to a variable depending on the input 2
+selection(num)==s
   end
 end
-
-def runner
-  prompt_user
-  selection(get_user_input)
-end
+#commented out the method because it isn't being called for this lab
+#def runner
+#  prompt_user
+#  selection(get_user_input)
+#end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,17 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+
+def snake_it_up(str) #changed argument to str instead of string because not sure if it will interfere with class string
+  str="surprise!" # initialized str with surprise
+  if str[0] == "s" # checking if the string starts with s
+    count=0  # using a do while loop to add s to the first letter 10 times, could not multiply an integer with string, so this method instead
+    while count <10 do
+      str[count]=str[0] + "s" # adding s to only the first letter in the array str[0]
+      count +=1
+    end
+str #return string str after adding s 10 times
+
   else
-    string
+    str
   end
 end
